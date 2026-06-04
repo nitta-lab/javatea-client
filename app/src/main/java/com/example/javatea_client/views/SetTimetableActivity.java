3e8cd3ad7da103a35c42791aa0630357a3df8761
r-hashime <s2371079@s.konan-u.ac.jp>
2026-06-04 16:14:36 +0900

    SetTimetableActivity.java：layoutにnavigationを追加 activity_set_timetable.xml：Navigationを設定

--- app/src/main/java/com/example/javatea_client/views/SetTimetableActivity.java
+++ app/src/main/java/com/example/javatea_client/views/SetTimetableActivity.java
@@ -22,5 +22,6 @@ public class SetTimetableActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Navigation.setup(this); //Navigationクラスを動かす
     }
 }
