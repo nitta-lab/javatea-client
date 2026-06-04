66872733a447f3c3172fab3a881b1600dcfc9644
r-hashime <s2371079@s.konan-u.ac.jp>
2026-06-04 17:42:01 +0900

    Activityごとの文字の反映ができるように、コードを変更しました。

--- app/src/main/java/com/example/javatea_client/views/ModeBar.java
+++ app/src/main/java/com/example/javatea_client/views/ModeBar.java
@@ -1,26 +1,17 @@
 package com.example.javatea_client.views;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.widget.TextView;
 
 import com.example.javatea_client.R;
 
-public class ModeBar extends AppCompatActivity {
+public class ModeBar {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_mode_bar);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    public static void setup(Activity activity, String text) {
+        TextView barText = activity.findViewById(R.id.modebar);
+        barText.setText(text);
+        barText.setTypeface(null, Typeface.BOLD);
     }
+
 }
