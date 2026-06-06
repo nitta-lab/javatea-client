8d8850c5f47c0341ae4e4b2bc1552d39464ee136
t-yokoyama <s2371093@s.konan-u.ac.jp>
2026-06-06 17:30:03 +0900

    models関係をしました。おそらくここからまた修正が入ります。

--- app/src/main/java/com/example/javatea_client/models/Department.java
+++ app/src/main/java/com/example/javatea_client/models/Department.java
@@ -1,4 +1,31 @@
 package com.example.javatea_client.models;
 
+import java.util.HashMap;
 public class Department {
+    private final String department_name;
+    private final HashMap<String, Lecture> lectureInDepartment;
+
+    public Department(String department_name) {
+        this.department_name = department_name;
+        this.lectureInDepartment = new HashMap<>();
+    }
+
+    public String getDepartmentName() {
+        return department_name;
+    }
+
+    public void addLectures(String lectureId, Lecture lecture) {
+        lectureInDepartment.put(lectureId, lecture);
+    }
+
+    public Lecture getLecture(String lectureId) {
+        if(!lectureInDepartment.containsKey(lectureId)) {
+            return null;
+        }
+        return lectureInDepartment.get(lectureId);
+    }
+
+    public HashMap<String, Lecture> getLectures() {
+        return lectureInDepartment;
+    }
 }
--- app/src/main/java/com/example/javatea_client/models/University.java
+++ app/src/main/java/com/example/javatea_client/models/University.java
@@ -1,4 +1,70 @@
 package com.example.javatea_client.models;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class University {
+    private final String univ_id;
+    private String name;
+    private String kana;
+    private final HashMap<String, Lecture> lectures = new HashMap<>();
+    private final HashMap<String, Faculty> faculties = new HashMap<>();
+    public University(String univ_id, String name, String kana) {
+        this.univ_id = univ_id;
+        this.name = name;
+        this.kana = kana;
+    }
+    public String getId() {
+        return univ_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKana() {
+        return kana;
+    }
+
+    public void setKana(String kana) {
+        this.kana = kana;
+    }
+
+    public Faculty createFaculty(String faculty_name) {
+        if(this.faculties.containsKey(faculty_name)){
+            return null;
+        }
+        faculties.put(faculty_name, new Faculty(faculty_name));
+        return faculties.get(faculty_name);
+    }
+
+    public Faculty getFaculty(String faculty_name) {
+        if(!faculties.containsKey(faculty_name)) {
+            return null;
+        }
+        return faculties.get(faculty_name);
+    }
+
+    public Set<String> getFaculties() {
+        return faculties.keySet();
+    }
+
+    public void addLecture(String lecture_id, Lecture lecture) {
+        lectures.put(lecture_id, lecture);
+    }
+
+    public Lecture getLecture(String lecture_id) {
+        if (!lectures.containsKey(lecture_id)) {
+            return null;
+        }
+        return lectures.get(lecture_id);
+    }
+
+    public HashMap<String, Lecture> getLectures() {
+        return lectures;
+    }
 }
