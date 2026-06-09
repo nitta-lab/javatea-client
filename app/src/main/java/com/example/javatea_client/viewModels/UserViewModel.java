e5b113d47a9e8b1a7a6507b56f1e9b67e51ecb28
h-oonishi <s2371054@s.konan-u.ac.jp>
2026-06-09 14:34:43 +0900

    viewmodel作成中

--- app/src/main/java/com/example/javatea_client/viewModels/UserViewModel.java
+++ app/src/main/java/com/example/javatea_client/viewModels/UserViewModel.java
@@ -1,5 +1,7 @@
 package com.example.javatea_client.viewModels;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -7,7 +9,9 @@ import androidx.lifecycle.ViewModel;
 import com.example.javatea_client.models.User;
 import com.example.javatea_client.resources.UserResource;
 
+import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class UserViewModel extends ViewModel {
@@ -21,9 +25,11 @@ public class UserViewModel extends ViewModel {
 
     public UserViewModel(){
         this.retrofit = new Retrofit.Builder()
-                .baseUrl("http://nitta-lab-www.is.konan-u.ac.jp/javatea/")
+                .baseUrl("http://localhost:8080/")
+                //.addConverterFactory(ScalarsConverterFactory.create())
+                //.addConverterFactory(JacksonConverterFactory.create())
                 .build();
-                this.userResource = retrofit.create(UserResource.class);
+        this.userResource = retrofit.create(UserResource.class);
     }
 
     public LiveData<User> getUser() {
@@ -41,5 +47,55 @@ public class UserViewModel extends ViewModel {
     public static LiveData<String> getError() {
         return error;
     }
+
+    public void createUser(String id, String name ,String password) {
+        loading.setValue(true);
+        userResource.createUser(id, name, password).enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> c, Response<User> res) {
+                loading.setValue(false);
+                if (res.isSuccessful() && res.body() != null) {
+                    User u = res.body();
+                    Log.d("API_RESPONSE", u.getUid());
+                    user.setValue(res.body());
+                } else {
+                    if (res.code() == 409) {
+                        error.setValue("このユーザーIDは既に使用されています");
+                        user.setValue(null);
+                    } else {
+                        Log.e("API_RESPONSE", "Response error code: " + res.code());
+                        error.setValue("登録失敗: " + res.code());
+                        user.setValue(null);
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call<User> c, Throwable t) {
+                loading.setValue(false);
+                error.setValue("エラー: " + t.getMessage());
+            }
+        });
+    }
+
+    public void login(String uid, String pw) {
+        loading.setValue(true);
+        userResource.login(uid, pw).enqueue(new Callback<String>(){
+            @Override
+            public void onResponse(Call<String> c,Response<String> res) {
+                if (res.isSuccessful()) {
+                    token.setValue(res.body());
+                } else {
+                    error.setValue("ログイン失敗: " + res.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> c, Throwable t) {
+                loading.setValue(false);
+                error.setValue("エラー: " + t.getMessage());
+            }
+        });
+    }
+
 
 }
