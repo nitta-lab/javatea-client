eace7b7f0072f94613901ab16b60dcd2b8efc9f0
k-nomura <s2371086@s.konan-u.ac.jp>
2026-06-09 16:25:34 +0900

    一旦プッシュします

--- app/src/main/java/com/example/javatea_client/views/LoginActivity.java
+++ app/src/main/java/com/example/javatea_client/views/LoginActivity.java
@@ -7,10 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.javatea_client.R;
+import com.example.javatea_client.viewModels.UserViewModel;
 
 public class LoginActivity extends AppCompatActivity {
+
+    UserViewModel userViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +26,7 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        ModeBar.setup(this, "ログイン");
     }
 }
