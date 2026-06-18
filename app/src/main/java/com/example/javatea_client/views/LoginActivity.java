75863c4ee6e890187c02e4966a1e621017f095fa
k-nomura <s2371086@s.konan-u.ac.jp>
2026-06-18 18:16:31 +0900

    一旦プッシュします

--- app/src/main/java/com/example/javatea_client/views/LoginActivity.java
+++ app/src/main/java/com/example/javatea_client/views/LoginActivity.java
@@ -3,8 +3,10 @@ package com.example.javatea_client.views;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -16,8 +18,9 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.javatea_client.R;
-//import com.example.javatea_client.Javatea;
+import com.example.javatea_client.Javatea;
 import com.example.javatea_client.viewModels.UserViewModel;
+import com.google.android.material.textfield.*;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -35,19 +38,39 @@ public class LoginActivity extends AppCompatActivity {
         });
         userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
 
-        EditText userIdEditText = findViewById(R.id.userid_edit);
-        EditText passwordEditText = findViewById(R.id.password_edit);
+        TextInputEditText userIdEditText = findViewById(R.id.userid_edit);
+        TextInputEditText passwordEditText = findViewById(R.id.password_edit);
+        TextInputLayout userIdLayout = findViewById((R.id.userid_layout));
+        TextInputLayout passwordLayout = findViewById((R.id.password_layout));
         Button loginButton = findViewById(R.id.login_button);
         Button signupButton = findViewById(R.id.signup_button);
+        TextView errorText = findViewById(R.id.error_textView);
         ModeBar.setup(this, "ログイン");
 
-        UserViewModel.getError().observe(this, errorMsg -> {
+        userViewModel.getError().observe(this, errorMsg -> {
             if (errorMsg != null && !errorMsg.isEmpty()) {
-                Toast.makeText(this, errorMsg, Toast.LENGTH_SHORT).show();
+                switch(errorMsg) {
+                    case"ログイン失敗: 400":
+                    case"ログイン失敗: 401":
+                    case"ログイン失敗: 404":
+                        errorText.setText("※IDまたはパスワードが正しくありません");
+                        break;
+
+                    case "ログイン失敗: 500":
+                        errorText.setText("※サーバーエラーが発生しました");
+                        break;
+
+                    default:
+                        errorText.setText("※ログインに失敗しました");
+                }
+
+                errorText.setVisibility(View.VISIBLE);
+                userIdLayout.setBackgroundResource(R.drawable.input_textbox_error);
+                passwordLayout.setBackgroundResource(R.drawable.input_textbox_error);
             }
         });
 
-        userViewModel.getToken.observe(this, new Observer<String>() {
+        userViewModel.getToken().observe(this, new Observer<String>() {
 
             @Override
             public void onChanged(String token) {
@@ -56,7 +79,7 @@ public class LoginActivity extends AppCompatActivity {
                 }
 
                 if (!token.isEmpty()) {
-                    //Javatea app = (Javatea) LoginActivity.this.getApplication();
+                    Javatea app = (Javatea) LoginActivity.this.getApplication();
                     app.setToken(token);
 
                     Intent intent = new Intent(LoginActivity.this, TimetableActivity.class);
@@ -70,10 +93,34 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         loginButton.setOnClickListener(view -> {
-            String userId = userIdEditText.getText().toString();
+            String userId = userIdEditText.getText().toString().trim();
             String password = passwordEditText.getText().toString();
 
-            userViewModel.login(userId, password);
+            errorText.setError(null);
+            errorText.setVisibility(View.GONE);
+            userIdLayout.setBackgroundResource(R.drawable.input_textbox);
+            passwordLayout.setBackgroundResource(R.drawable.input_textbox);
+
+            if (userId.isEmpty() && password.isEmpty()) {
+                errorText.setText("※ユーザーIDとパスワードを入力してください");
+                errorText.setVisibility(View.VISIBLE);
+                userIdLayout.setBackgroundResource(R.drawable.input_textbox_error);
+                passwordLayout.setBackgroundResource(R.drawable.input_textbox_error);
+                return;
+            }
+            else if (userId.isEmpty()) {
+                errorText.setText("※ユーザーIDを入力してください");
+                errorText.setVisibility(View.VISIBLE);
+                userIdLayout.setBackgroundResource(R.drawable.input_textbox_error);
+            }
+            else if (password.isEmpty()) {
+                errorText.setText("※パスワードを入力してください");
+                errorText.setVisibility(View.VISIBLE);
+                passwordLayout.setBackgroundResource(R.drawable.input_textbox_error);
+            }
+            else {
+                userViewModel.login(userId, password);
+            }
         });
 
         signupButton.setOnClickListener(view -> {
