2380abc1f713e2a00738730ba19d49d7725b48b7
s-machida <s2371089@s.konan-u.ac.jp>
2026-06-20 15:35:32 +0900

    その他の授業を表示するotherLecturesActivityを作成しました

---
+++ app/src/main/java/com/example/javatea_client/views/OtherLecturesActivity.java
@@ -0,0 +1,26 @@
+package com.example.javatea_client.views;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.javatea_client.R;
+
+public class OtherLecturesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_other_lectures);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
