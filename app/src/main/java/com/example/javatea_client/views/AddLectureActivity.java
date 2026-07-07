0e140422a2b99fc592546e20581dedbe2b2130e9
m-nakasu <s2371098@s.konan-u.ac.jp>
2026-07-07 15:43:37 +0900

    更新します

--- app/src/main/java/com/example/javatea_client/views/AddLectureActivity.java
+++ app/src/main/java/com/example/javatea_client/views/AddLectureActivity.java
@@ -3,6 +3,7 @@ package com.example.javatea_client.views;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -19,6 +20,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.javatea_client.viewModels.CategoryViewModel;
 import com.example.javatea_client.viewModels.LectureViewModel;
 
 
@@ -32,6 +36,21 @@ public class AddLectureActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        lectureViewModel = new LectureViewModel();
+        lectureViewModel = new ViewModelProvider(this).get(LectureViewModel.class);
+//        lectureViewModel.getFinishAddLecture().observe(this, finishAddLecture -> {
+//            // nullかfalseはreturn
+//            if(finishAddLecture == null || !finishAddLecture) {
+//                return;
+//            }
+//            // trueなら合図出す
+//            setResult(RESULT_OK);
+//
+//            // 画面閉じる
+//            finish();
+//        });
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_lecture);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -51,13 +70,11 @@ public class AddLectureActivity extends AppCompatActivity {
         Spinner spinner1 = findViewById(R.id.spinner1);
         Spinner spinner2 = findViewById(R.id.spinner2);
 
-
         //コマ数選択(プルダウン)
         String[] classeslist = {"1コマ","2コマ","3コマ"};
         ArrayAdapter<String> adapter1 = new ArrayAdapter<>(AddLectureActivity.this, android.R.layout.simple_spinner_item,classeslist);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter1);
-
 
         //学年選択(プルダウン)
         String[] gradelist = {"1年次以上","2年次以上","3年次以上","4年次以上"};
@@ -66,12 +83,18 @@ public class AddLectureActivity extends AppCompatActivity {
         spinner2.setAdapter(adapter2);
 
 
-        //前期後期の切り替え
+        //前期後期その他の切り替え
         RadioButton FirstButton = findViewById(R.id.FirstButton);
+        RadioButton AllyearButton = findViewById(R.id.AllyearButton);
         if("前期".equals(semester)){
             FirstButton.setText("前期");
+            AllyearButton.setEnabled(true);
         }else if("後期".equals(semester)){
             FirstButton.setText("後期");
+            AllyearButton.setEnabled(true);
+        }else if("その他".equals(semester)){
+            FirstButton.setText("その他");
+            AllyearButton.setEnabled(false);
         }
 
 
@@ -81,10 +104,10 @@ public class AddLectureActivity extends AppCompatActivity {
         AddButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                RadioGroup RadioButtonMajor = findViewById(R.id.RadioGroupMajor);
+                RadioGroup RadioGroupMajor = findViewById(R.id.RadioGroupMajor);
                 RadioGroup RadioButtonSemester = findViewById(R.id.RadioGroupSemester);
 
-                int majorId = RadioButtonMajor.getCheckedRadioButtonId();
+                int majorId = RadioGroupMajor.getCheckedRadioButtonId();
                 int semesterId = RadioButtonSemester.getCheckedRadioButtonId();
 
                 if(majorId == -1){
@@ -96,30 +119,42 @@ public class AddLectureActivity extends AppCompatActivity {
                     return;
                 }
 
-                RadioButton selectedMajor = findViewById(majorId);
-                RadioButton selectedsemester = findViewById(semesterId);
 
-                String major = selectedMajor.getText().toString();
+                RadioButton selectedMajor = findViewById(majorId);
+                String type = selectedMajor.getText().toString();
+                Log.d("addLecture", "type確認：" + type);
+
+                RadioButton selectedsemester = findViewById(semesterId);
                 String semester = selectedsemester.getText().toString();
 
 
-                String classes = spinner1.getSelectedItem().toString();
-                String grade = spinner2.getSelectedItem().toString();
+                //String classes = spinner1.getSelectedItem().toString();
+                //String grade = spinner2.getSelectedItem().toString();
 
                 //授業名入力
                 //入力しているかチェック
-                EditText name = findViewById(R.id.LectureEditText);
-                String inputText = name.getText().toString();
-                if (inputText.isEmpty()) {
-                    name.setError("入力してください");
+                EditText lectureEditText = findViewById(R.id.LectureEditText);
+                String name = lectureEditText.getText().toString().trim();
+
+                if (name.isEmpty()) {
+                    lectureEditText.setError("入力してください");
+                    lectureEditText.requestFocus();
                     return;
                 }
 
-                //LectureViewModel.crateLecture(name, grade, semester, classes, day, period);
+
+                int classes = spinner1.getSelectedItemPosition() + 1;
+                int grade = spinner2.getSelectedItemPosition() + 1;
 
 
-                Intent intent = new Intent(AddLectureActivity.this, SetTimetableActivity.class);
-                startActivity(intent);
+
+                Log.d("AddLectureActivity", "名前" + name + "曜日" + day + "時限" + period );
+                //LectureViewModel.createLecture(name, grade, "前期", classes, "水", period);
+//                lectureViewModel.startAddLecture(name, grade, semester, classes, day, period, univId, facultyName, departmentName, type);
+//                lectureViewModel.startAddLecture("最適化", 2, "前期", 1, "水", 3, "univ-id1", "知能情報学部", "知能情報学科", "faculty");
+
+
+                finish();
             }
 
         });
@@ -129,10 +164,8 @@ public class AddLectureActivity extends AppCompatActivity {
         CloseButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                Intent intent = new Intent(AddLectureActivity.this, SetTimetableActivity.class);
-                startActivity(intent);
 
-                //finish();
+                finish();
             }
         });
     }
