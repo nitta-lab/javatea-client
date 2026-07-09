d9bff3d59c2f45d5984f77e2764ab8d0c3b23ed6
h-maeda <s2371087@s.konan-u.ac.jp>
2026-07-09 17:46:40 +0900

    途中まで一旦コミット＆プッシュ

--- app/src/main/java/com/example/javatea_client/viewModels/QuestionViewModel.java
+++ app/src/main/java/com/example/javatea_client/viewModels/QuestionViewModel.java
@@ -1,18 +1,26 @@
 package com.example.javatea_client.viewModels;
 
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.javatea_client.models.Question;
 import com.example.javatea_client.resources.QuestionResource;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class QuestionViewModel extends ViewModel {
-    //サーバー呼び出す用のリソース
-    private final QuestionResource questionResource;
+    private Retrofit retrofit;
+    private final QuestionResource questionResource; //サーバー呼び出す用のリソース
     public QuestionViewModel(){
-        Retrofit retrofit = new Retrofit.Builder()
+        this.retrofit = new Retrofit.Builder()
                 .baseUrl("http://nitta-lab-www.is.konan-u.ac.jp/javatea-server/")
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(JacksonConverterFactory.create())
