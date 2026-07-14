96c7437c1e4c0cb3aa4e4d079441902b76c6e526
s-machida <s2371089@s.konan-u.ac.jp>
2026-07-14 13:59:32 +0900

    AnswerModelとAnswerResourceとAnswerViewModelを作成しました

---
+++ app/src/main/java/com/example/javatea_client/viewModels/AnswerViewModel.java
@@ -0,0 +1,69 @@
+package com.example.javatea_client.viewModels;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.javatea_client.models.Answer;
+import com.example.javatea_client.resources.AnswerResource;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+import java.util.*;
+public class AnswerViewModel extends ViewModel {
+
+    private final Retrofit retrofit;
+    private final AnswerResource answerResource;
+
+    private final MutableLiveData<HashMap<String, Answer>> answers = new MutableLiveData<>();
+
+    private final MutableLiveData<Answer> answer = new MutableLiveData<>();
+
+    private final MutableLiveData<String> error = new MutableLiveData<>();
+    public AnswerViewModel(){
+        this.retrofit = new Retrofit.Builder()
+                .baseUrl("http://nitta-lab-www.is.konan-u.ac.jp/javatea-server/")
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(JacksonConverterFactory.create())
+                .build();
+        this.answerResource = retrofit.create(AnswerResource.class);
+    }
+
+    public LiveData<HashMap<String,Answer>> getAnswers(){
+        return answers;
+    }
+
+    public LiveData<Answer> getAnswer(){
+        return answer;
+    }
+
+    public void createAnswer(String qid,String uid,String body,String token){
+        answerResource.createAnswer(qid,uid,body,token)
+                .enqueue(new Callback<Answer>() {
+                    @Override
+                    public void onResponse(Call<Answer> call, Response<Answer> response) {
+                        if(response.isSuccessful()&&response.body() != null){
+                            answer.setValue(response.body());
+                        }else{
+                            if(response.code() == 404){
+                                error.setValue("ユーザが存在しません");
+                            }else if(response.code() == 401){
+                                error.setValue("認証エラー");
+                            }else{
+                                error.setValue("answerの作成に失敗");
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Answer> call, Throwable throwable) {
+
+                    }
+                });
+    }
+}
