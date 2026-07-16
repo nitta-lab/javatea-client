c01d5da36be8cfc3c8c5ff172ad72c33783a5fe4
s-machida <s2371089@s.konan-u.ac.jp>
2026-07-16 14:49:23 +0900

    ViewAnswerFragment変更

--- app/src/main/java/com/example/javatea_client/views/ViewAnswerFragment.java
+++ app/src/main/java/com/example/javatea_client/views/ViewAnswerFragment.java
@@ -2,59 +2,40 @@ package com.example.javatea_client.views;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.javatea_client.Javatea;
 import com.example.javatea_client.R;
+import com.example.javatea_client.models.Answer;
+import com.example.javatea_client.viewModels.AnswerViewModel;
+import com.example.javatea_client.viewModels.UserViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ViewAnswerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ViewAnswerFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private AnswerViewModel answerViewModel;
+    private String userId;
+    private String token;
+    private Answer displayAnswer;
+    private TextView answererTextView;
+    private TextView answerBodyTextView;
 
     public ViewAnswerFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ViewAnswerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ViewAnswerFragment newInstance(String param1, String param2) {
-        ViewAnswerFragment fragment = new ViewAnswerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +43,57 @@ public class ViewAnswerFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_view_answer, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //AnswerViewModelの取得
+        answerViewModel = new ViewModelProvider(requireActivity()).get(AnswerViewModel.class);
+
+        LectureListActivity activity = (LectureListActivity) requireActivity();
+
+        //前の画面から情報を取得
+        String qid = getArguments().getString("qid");
+        String aid = getArguments().getString("aid");
+
+        //Javateaから情報を取得
+        Javatea javatea = (Javatea) requireActivity().getApplication();
+        userId = javatea.getUserId();
+        token = javatea.getToken();
+
+        //指定aidのAnswerを取得する
+        answerViewModel.loadAnswer(qid,aid,userId,token);
+
+        answerViewModel.getAnswer().observe(getViewLifecycleOwner(), new Observer<Answer>() {
+            @Override
+            public void onChanged(Answer answer) {
+                displayAnswer = answer;
+                setAnswerer(view);
+            }
+        });
+        setAnswerer(view);
+
+        Button backButton = view.findViewById(R.id.BackButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //画面遷移
+                //getParentFragmentManager().beginTransaction().replace(R.id.fragment_container)
+            }
+        });
+    }
+
+    private void setAnswerer(View view){
+        answererTextView = view.findViewById(R.id.answer);
+        if(displayAnswer == null){
+            answererTextView.setText("");
+            answerBodyTextView.setText("");
+        }else{
+            String answerer = displayAnswer.getUid();
+            answererTextView.setText(answerer + "さんの回答");
+            answerBodyTextView.setText("Answer : \n" + displayAnswer.getBody());
+        }
     }
 }
