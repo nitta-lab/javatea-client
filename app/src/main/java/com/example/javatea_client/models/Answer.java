8542aab5136f6056118b0d9413dcf893d38399c8
s-machida <s2371089@s.konan-u.ac.jp>
2026-07-16 15:08:55 +0900

    Answer周りを修正しました。(nameを追加した)

--- app/src/main/java/com/example/javatea_client/models/Answer.java
+++ app/src/main/java/com/example/javatea_client/models/Answer.java
@@ -4,12 +4,14 @@ public class Answer {
     private String aid;
     private String body;
     private String uid;
+    private String name;
 
     //コンストラクタ
-    public Answer(String aid, String body,String uid) {
+    public Answer(String aid, String body,String uid,String name) {
         this.aid = aid;
         this.body = body;
         this.uid = uid;
+        this.name = name;
     }
 
     public String getAid() {return aid;} //aidを返す(念のため)
@@ -17,5 +19,7 @@ public class Answer {
     public String getBody() {return body;}
 
     public String getUid() {return uid;}
+
+    public String getName(){return name;}
 }
 
