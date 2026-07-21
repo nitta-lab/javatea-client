20e56845016f0655b74b08a423e87c8a94e4e769
r-hashime <s2371079@s.konan-u.ac.jp>
2026-07-21 13:30:16 +0900

    MyPage用のActivityとxmlファイルを追加しました。

---
+++ app/src/main/java/com/example/javatea_client/views/MyPageActivity.java
@@ -0,0 +1,30 @@
+package com.example.javatea_client.views;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.javatea_client.R;
+
+public class MyPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_my_page_activity);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // 他のActivityから画面を取得
+        Navigation.setup(this); //Navigationクラスを動かす
+        ModeBar.setup(this, "時間割設定"); //ModeBarを設定
+    }
+}
